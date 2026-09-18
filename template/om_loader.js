cdfd227c5bd1bdaa3babae3987fd82fa11ffaa49
Apemiro <1157775623@qq.com>
2026-09-18 17:26:11 +0800

    aside nav selectAll

--- template/om_loader.js
+++ template/om_loader.js
@@ -1,3 +1,21 @@
-function toggle(sender){
+function nav_toggle(sender){
     sender.classList.toggle('active')
+}
+function nav_selectAll(sender, event){
+    event.preventDefault();
+    event.stopPropagation();
+    const details = sender.closest('details');
+    const items = details.querySelectorAll('li a');
+	for(const a of items){
+        a.classList.add('active');
+    }
+}
+function nav_deselectAll(sender, event){
+    event.preventDefault();
+    event.stopPropagation();
+    const details = sender.closest('details');
+    const items = details.querySelectorAll('li a');
+	for(const a of items){
+        a.classList.remove('active');
+    }
 }
